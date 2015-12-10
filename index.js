1e546ffa84967ee7ff9f13ee9f79b96299aae458
sunke <sunkejl@live.com>
2015-12-10 19:22:09 +0800

    npm

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var redis = require('redis');
+var db = redis.createClient();
+app.use(function(req, res, next){
+  var ua = req.headers['user-agent'];
+  db.zadd('online', Date.now(), ua, next);
+});
+app.use(function(req, res, next){
+  var min = 60 * 1000;
+  var ago = Date.now() - min;
+  db.zrevrangebyscore('online', '+inf', ago, function(err, users){
+    if (err) return next(err);
+    req.online = users;
+    next();
+  });
+});
+
+
+
+
+app.set('view engine','jade');
+app.listen(3000);
+
+app.get('/', function (req, res) {
+  res.render('index',{title:'sunke'});
+});
+
+app.get('/redis', function (req, res) {
+  res.send(req.online.length + ' users 123 online');
+});
