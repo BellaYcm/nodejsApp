08f54933106b57240ffacc6cd36ac9f06011ff52
sunke <sunkejl@live.com>
2015-12-16 13:21:48 +0800

    refs 1216

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+/**
+ * Created by sunke on 2015/12/11.
+ */
 var express = require('express');
 var app = express();
 var redis = require('redis');
 var db = redis.createClient();
 app.use(function(req, res, next){
-  var ua = req.headers['user-agent'];
-  db.zadd('online', Date.now(), ua, next);
+    var ua = req.headers['user-agent'];
+    db.zadd('online', Date.now(), ua, next);
 });
 app.use(function(req, res, next){
-  var min = 60 * 1000;
-  var ago = Date.now() - min;
-  db.zrevrangebyscore('online', '+inf', ago, function(err, users){
-    if (err) return next(err);
-    req.online = users;
-    next();
-  });
+    var min = 60 * 1000;
+    var ago = Date.now() - min;
+    db.zrevrangebyscore('online', '+inf', ago, function(err, users){
+        if (err) return next(err);
+        req.online = users;
+        next();
+    });
 });
 
 
@@ -23,9 +26,9 @@ app.set('view engine','jade');
 app.listen(3000);
 
 app.get('/', function (req, res) {
-  res.render('index',{title:'sunke'});
+    res.render('pages/index',{title:'sunke',movies:[{title:123},{title:222}]});
 });
 
 app.get('/redis', function (req, res) {
-  res.send(req.online.length + ' users 123 online');
+    res.send(req.online.length + ' users 123 online');
 });
